e6ff8343276f7f8dc12b2e88307d883bd75a1554
Lucas Altenhofen <lucas.a.bertrand@gmail.com>
2021-11-28 18:30:24 -0300

    Fix orders controller

--- src/modules/orders/infra/http/controller/OrdersController.ts
+++ src/modules/orders/infra/http/controller/OrdersController.ts
@@ -7,10 +7,22 @@ import FindOrderService from '@modules/orders/services/FindOrderService';
 
 export default class OrdersController {
   public async show(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { id } = request.params;
+
+    const findOrderService = container.resolve(FindOrderService);
+
+    const order = await findOrderService.execute({ id });
+
+    return response.json(order);
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { customer_id, products } = request.body;
+
+    const createOrderService = container.resolve(CreateOrderService);
+
+    const order = await createOrderService.execute({ customer_id, products });
+
+    return response.json(order);
   }
 }
