63bb08a15549ad3391465302359cccf1585a0e8d
Lucas Altenhofen <lucas.a.bertrand@gmail.com>
2021-11-28 17:16:38 -0300

    Add customer id to orders migration

---
+++ src/shared/infra/typeorm/migrations/1638130187873-AddCustomerIdToOrders.ts
@@ -0,0 +1,36 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddCustomerIdToOrders1638130187873
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders',
+      new TableColumn({
+        name: 'customer_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders',
+      new TableForeignKey({
+        name: 'OrdersCustomerFK',
+        columnNames: ['customer_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'customers',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('orders', 'OrdersCustomerFK');
+    await queryRunner.dropColumn('orders', 'customer_id');
+  }
+}
