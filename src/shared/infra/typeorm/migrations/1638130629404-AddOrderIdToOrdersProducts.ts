fc66cff1770850d02f266e9f83a79e9ce8bce60d
Lucas Altenhofen <lucas.a.bertrand@gmail.com>
2021-11-28 17:20:08 -0300

    Add order id to orders products migration

---
+++ src/shared/infra/typeorm/migrations/1638130629404-AddOrderIdToOrdersProducts.ts
@@ -0,0 +1,39 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class AddOrderIdToOrdersProducts1638130629404
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'orders_products',
+      new TableColumn({
+        name: 'order_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'orders_products',
+      new TableForeignKey({
+        name: 'OrdersProductsOrderFK',
+        columnNames: ['order_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'orders',
+        onDelete: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      'orders_products',
+      'OrdersProductsOrderFK',
+    );
+    await queryRunner.dropColumn('orders_products', 'order_id');
+  }
+}
