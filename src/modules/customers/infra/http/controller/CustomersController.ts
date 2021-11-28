d91d6481aef327b93e55c552329c7108a216d41b
Lucas Altenhofen <lucas.a.bertrand@gmail.com>
2021-11-28 17:33:03 -0300

    Fix customer controller

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -6,6 +6,12 @@ import { container } from 'tsyringe';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+
+    const createCustomerService = container.resolve(CreateCustomerService);
+
+    const customer = createCustomerService.execute({ name, email });
+
+    return response.json(customer);
   }
 }
